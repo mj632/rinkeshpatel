ad88cb5241ed55fbadee7f980abe6aaa7f7915e2
DESKTOP-V66GIGC\Manthan <mj632>
2018-05-24 02:02:05 -0500

    Final changes done website completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,16 @@ $(document).ready(function () {
         goTo($(this));
     });
 
+    $('.nav-link').click(function () {
+        goTo($(this));
+    });
+
+    $('#nav-toggle-btn').blur(function(){
+        goTo($(this));
+    });
+    
+
+
     var goTo = function (window) {
         var id = window[0].hash;
         var scrollTopHeight = $(id).offset().top - $('#page-navbar')[0].clientHeight;
@@ -79,5 +89,132 @@ $(document).ready(function () {
         }
 
     };
+
+    //============Highchart starts from here===========//
+
+    function renderIcons() {
+
+        // Move icon
+        if (!this.series[0].icon) {
+            this.series[0].icon = this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
+                .attr({
+                    'stroke': '#303030',
+                    'stroke-linecap': 'round',
+                    'stroke-linejoin': 'round',
+                    'stroke-width': 2,
+                    'zIndex': 10
+                });
+        }
+        this.series[0].icon.translate(
+            this.chartWidth / 2 - 10,
+            this.plotHeight / 2 - this.series[0].points[0].shapeArgs.innerR -
+                (this.series[0].points[0].shapeArgs.r - this.series[0].points[0].shapeArgs.innerR) / 2
+        );
+    }
+
+var idList = ['Siemens-NX-container',
+              'SolidWorks-container',
+              'CREO-container',
+              'CATIA-container',
+              'DFM-container',
+              'Arduino-container',
+              '3DPrinting-container',
+              'Photoshop-container',
+              'Microsoft-Office-container'];
+var nameList = ['Siemens NX',
+                'SolidWorks',
+                'CREO 2.0',
+                'CATIA V5',
+                'DFM&A, GD & T',
+                'Arduino',
+                '3D Printing',
+                'Photoshop',
+                'Microsoft Office'];
+var skillScaleList = [80,72,65,60,70,40,80,70,60];
+
+//===============Highcharts for all the skills
+for(var i = 0; i < idList.length; i++) {
+    Highcharts.chart(idList[i], {
+      chart: {
+          type: 'solidgauge',
+          height: '110%',
+          events: {
+              render: renderIcons
+          }
+      },
+
+      credits: {
+          enabled: false
+      },
+
+      title: {
+          text: nameList[i],
+          style: {
+               fontSize: '1em',
+               fontWeight: 'bold'
+          },
+          align: 'center',
+          verticalAlign: 'bottom'
+
+      },
+
+      tooltip: {
+          borderWidth: 0,
+          backgroundColor: 'none',
+          shadow: false,
+          style: {
+              fontSize: '1em'
+          },
+          pointFormat: '{series.name}<br><span class = "d-none" style="font-size:1em; color: {point.color}; font-weight: bold">{point.y}%</span>',
+          positioner: function (labelWidth) {
+              return {
+                  x: (this.chart.chartWidth - labelWidth) / 2,
+                  y: (this.chart.plotHeight / 2) + 15
+              };
+          }
+      },
+
+      pane: {
+          startAngle: 0,
+          endAngle: 360,
+          background: [{ // Track for Move
+              outerRadius: '112%',
+              innerRadius: '88%',
+              backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[0])
+                  .setOpacity(0.3)
+                  .get(),
+              borderWidth: 0
+          }]
+      },
+
+      yAxis: {
+          min: 0,
+          max: 100,
+          lineWidth: 0,
+          tickPositions: []
+      },
+
+      plotOptions: {
+          solidgauge: {
+              dataLabels: {
+                  enabled: false
+              },
+              linecap: 'round',
+              stickyTracking: false,
+              rounded: true
+          }
+      },
+
+      series: [{
+          name: ' ',
+          data: [{
+              color: Highcharts.getOptions().colors[0],
+              radius: '112%',
+              innerRadius: '88%',
+              y: skillScaleList[i]
+          }]
+      }]
+    });
+  }
 });
 
